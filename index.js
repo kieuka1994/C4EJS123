b183a502a241517dbf9e766caa38dd66ee8f2262
kieuka1994 <91740828+kieuka1994@users.noreply.github.com>
2021-10-03 05:34:05 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,69 @@
+// STUDY 
+
+/*
+1. There diferent examples of invalid name
+- first number 
+- 1number 
+- the first character must not be a digit.
+
+2. Check a variable data types: typeof 
+
+3. 1-b; 2-a; 3-e; 4-c
+*/ 
+// SERIOU EXERCICES 
+
+// 3a. 
+let message = 'coding is great';
+console.log(message);
+
+// 3b. 
+let studentCount = 0;
+console.log(studentCount);
+
+// 4a.
+let message = 'coding is geat'
+message = 'coding might not be easy, but still great';
+console.log(message)
+
+// 4b. 
+let studentCount = 0;
+studentCount = 16; 
+console.log(studentCount);
+
+// 4c.
+let message ='coding is great';
+message = 'coding might not be easy, but still great';
+console.log(message.toLocaleLowerCase);
+
+// 4d. 
+let studentCount = 0;
+studentCount = 16; 
+studentCount += 1;
+console.log(studentCount);
+
+// 5.
+alert(`you look beautiful today`);
+
+// 6.
+let x = prompt ('hi there, your name please');
+alert(`hi $ {x}`);
+
+// 7.
+let firstname = prompt ('enter Sherlock');
+let lastname = prompt('enter Home');
+alert(`hi $ {Sherlock} ${Home}`);
+
+// 8. 
+let square = prompt('Enter the square edge of square');
+square *= square;
+alert(`the square area is ${edge}`);
+
+// 9. 
+let radius = prompt('enter the radius of the circle');
+radius = Math.diameter * Math.PI / 4;
+alert(`the circle area is ${radius}`);
+
+// 10. 
+let temperatureCel = prompt('enter the temperature in celcius');
+let temperatureFah = [(temperatureCel * 1.8) + 32];
+alert(`${temperatureCel} (C) = ${temperatureFah} (F)`);
